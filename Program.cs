d9dd8ae8372504bb22fd380a615d6cfdf19e25bc
kirilenkopavel <83178004+kirilenkopavel@users.noreply.github.com>
2022-09-19 23:48:05 +0400

    Реализовал код программы

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿String[] array = {"Hello", "123", "start", "Git"};
+String[] resultArray = new String[array.Length];
+
+// Функция сравнивает длину строки {str} со значением {value} и возвращает логиеское значение
+bool checkLenghtString(String str, int value) {
+    if(str.Length >= value) {
+        return true;
+    }
+    else {
+        return false;
+    }    
+}
+
+String[] checkArrayString(String[] array) {
+    int counter = 0;
+    for(int i = 0; i < array.Length; i++) {
+        if(checkLenghtString(array[i], 3)) {
+            resultArray[counter] = array[i];
+            counter++;
+        }
+    }
+    return resultArray;    
+}
+
+Console.WriteLine(checkArrayString(array));
+
